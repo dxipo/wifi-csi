ee4be0279c73858f8ede3ca0548e997d1a61d940
dxipo <ckingkong9@gmail.com>
2026-05-16 15:22:33 +0800

    Recover result 42 SDP power xfall baseline config

--- configs/wifi/petr_wifi.py
+++ configs/wifi/petr_wifi.py
@@ -1,15 +1,5 @@
 dataset_type = 'opera.WifiPoseDataset'
 data_root = '../data/wifipose'
-# train_pipeline = [
-#     dict(
-#         type='opera.DefaultFormatBundle',
-#         extra_keys=['gt_keypoints', 'gt_labels']),
-#     dict(
-#         type='mmdet.Collect',
-#         keys=['img', 'gt_bboxes', 'gt_labels', 'gt_keypoints', 'gt_areas'],
-#         meta_keys=[])
-# ]
-
 train_pipeline = [
     dict(
         type='opera.DefaultFormatBundle',
@@ -19,7 +9,6 @@ train_pipeline = [
         keys=['img', 'gt_bboxes', 'gt_labels', 'gt_keypoints', 'gt_areas'],
         meta_keys=[])
 ]
-
 test_pipeline = [
     dict(
         type='mmdet.MultiScaleFlipAug',
@@ -33,19 +22,16 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=32, #32 256
-    workers_per_gpu=2, #2 8
+    samples_per_gpu=32,
+    workers_per_gpu=2,
     train=dict(
         type='opera.WifiPoseDataset',
-        dataset_root='/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_train_data_hold_out_sdp_offline',
+        dataset_root=
+        '/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_train_data_hold_out_sdp_offline_power_xfall',
         offline_sdp_dir='csi_sdp_offline',
         offline_sdp_ext='.npy',
         sdp_layout='wtn',
-        stft_cfg=dict(
-            nperseg=8,
-            noverlap=4,
-            nfft=16
-        ),
+        stft_cfg=dict(nperseg=8, noverlap=4, nfft=16),
         pipeline=[
             dict(
                 type='opera.DefaultFormatBundle',
@@ -60,15 +46,12 @@ data = dict(
         mode='train'),
     val=dict(
         type='opera.WifiPoseDataset',
-        dataset_root='/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_test_data_hold_out_sdp_offline',
+        dataset_root=
+        '/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_test_data_hold_out_sdp_offline_power_xfall',
         offline_sdp_dir='csi_sdp_offline',
         offline_sdp_ext='.npy',
         sdp_layout='wtn',
-        stft_cfg=dict(
-            nperseg=8,
-            noverlap=4,
-            nfft=16
-        ),
+        stft_cfg=dict(nperseg=8, noverlap=4, nfft=16),
         pipeline=[
             dict(
                 type='mmdet.MultiScaleFlipAug',
@@ -84,15 +67,12 @@ data = dict(
         mode='test'),
     test=dict(
         type='opera.WifiPoseDataset',
-        dataset_root='/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_test_data_hold_out_sdp_offline',
+        dataset_root=
+        '/home/xl/CSI/Person-in-WiFi-3D-repo/data/wifipose/all_single_test_data_hold_out_sdp_offline_power_xfall',
         offline_sdp_dir='csi_sdp_offline',
         offline_sdp_ext='.npy',
         sdp_layout='wtn',
-        stft_cfg=dict(
-            nperseg=8,
-            noverlap=4,
-            nfft=16
-        ),
+        stft_cfg=dict(nperseg=8, noverlap=4, nfft=16),
         pipeline=[
             dict(
                 type='mmdet.MultiScaleFlipAug',
@@ -106,26 +86,12 @@ data = dict(
                 ])
         ],
         mode='test'))
-
-#fp16 = dict(loss_scale='dynamic')# new added
 evaluation = dict(interval=1, metric='mpjpe')
 checkpoint_config = dict(interval=5, max_keep_ckpts=100)
-# checkpoint_config = dict(
-#     interval=5,              # 每5个epoch存一次常规checkpoint
-#     max_keep_ckpts=20,       # 常规checkpoint最多保留20个（覆盖最近100个epoch）
-#     save_best='mpjpe',       # 额外保存“最优mpjpe”的模型
-#     rule='less'              # mpjpe越小越好
-# )
-#evaluation = dict(interval=1, metric='mpjpe')
-
-#log_config = dict(interval=10, hooks=[dict(type='TextLoggerHook')])
 log_config = dict(
     interval=10,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='TensorboardLoggerHook')
-    ]
-)
+    hooks=[dict(type='TextLoggerHook'),
+           dict(type='TensorboardLoggerHook')])
 custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
@@ -158,17 +124,17 @@ model = dict(
         num_outs=4),
     bbox_head=dict(
         type='opera.PETRHead',
-        num_query=100,
+        num_query=81,
         num_classes=1,
         in_channels=2048,
         sync_cls_avg_factor=True,
-        #with_kpt_refine=True,
         with_kpt_refine=True,
         as_two_stage=True,
         num_keypoints=14,
         transformer=dict(
             type='opera.PETRTransformer',
             num_keypoints=14,
+            two_stage_num_proposals=81,
             encoder=dict(
                 type='mmcv.DetrTransformerEncoder',
                 num_layers=6,
@@ -238,44 +204,32 @@ model = dict(
             gamma=2.0,
             alpha=0.25,
             loss_weight=4.0),
-        #loss_kpt=dict(type='mmdet.MSELoss', loss_weight=70.0),
         loss_kpt=dict(type='mmdet.MSELoss', loss_weight=10.0),
         loss_kpt_rpn=dict(type='mmdet.MSELoss', loss_weight=10.0),
-        # loss_oks=dict(type='opera.OKSLoss', loss_weight=2.0),
-        # loss_hm=dict(type='opera.CenterFocalLoss', loss_weight=4.0),
-        # loss_kpt_refine=dict(type='mmdet.MSELoss', loss_weight=70.0),
-        # loss_oks_refine=dict(type='opera.OKSLoss', loss_weight=3.0)),
-
         loss_oks=dict(type='opera.OKSLoss', loss_weight=0.0),
         loss_hm=dict(type='opera.CenterFocalLoss', loss_weight=0.0),
         loss_kpt_refine=dict(type='mmdet.MSELoss', loss_weight=0.0),
         loss_oks_refine=dict(type='opera.OKSLoss', loss_weight=0.0)),
-
-
-
     train_cfg=dict(
         assigner=dict(
             type='opera.PoseHungarianAssigner',
             cls_cost=dict(type='mmdet.FocalLossCost', weight=4.0),
-            #kpt_cost=dict(type='opera.KptMSECost', weight=70.0),
             kpt_cost=dict(type='opera.KptMSECost', weight=10.0),
-            #oks_cost=dict(type='opera.OksCost', weight=7.0))),
             oks_cost=dict(type='opera.OksCost', weight=0.0))),
-    test_cfg=dict(max_per_img=100))
+    test_cfg=dict(max_per_img=81))
 optimizer = dict(
     type='AdamW',
-    lr=2e-05, # 2e-05 3e
+    lr=2e-05,
     weight_decay=0.0001,
     paramwise_cfg=dict(
         custom_keys=dict(
             backbone=dict(lr_mult=0.1),
             sampling_offsets=dict(lr_mult=0.1),
             reference_points=dict(lr_mult=0.1))))
-optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2)) # max_norm=0.1 1.0
-lr_config = dict(policy='step', step=[400]) #400
-runner = dict(type='EpochBasedRunner', max_epochs=450) #450
+optimizer_config = dict(grad_clip=dict(max_norm=0.1, norm_type=2))
+lr_config = dict(policy='step', step=[400])
+runner = dict(type='EpochBasedRunner', max_epochs=450)
 find_unused_parameters = True
-work_dir = '/home/xl/CSI/Person-in-WiFi-3D-repo/result/42_wifipose_2d_baseline_SDP_offline'
+work_dir = '/home/xl/CSI/Person-in-WiFi-3D-repo/result/42_wifipose_2d_baseline_SDP_offline_power_xfall'
 auto_resume = False
-#gpu_ids = range(0, 3)
-gpu_ids = range(0, 1)
+gpu_ids = [0]
